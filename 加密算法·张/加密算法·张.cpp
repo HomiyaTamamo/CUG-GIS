7f988f210deeda76a4d49d4f47ee785625ec73cb
HomiyaTamamo <167890854+HomiyaTamamo@users.noreply.github.com>
2026-03-12 20:26:19 +0800

    Add files via upload
    Signed-off-by: HomiyaTamamo <167890854+HomiyaTamamo@users.noreply.github.com>

---
+++ 加密算法·张/加密算法·张.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> read = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," " };
+    cout << "输入加密/解密密钥" << endl;
+    int input1, input2;
+    cin >> input1 >> input2;
+    bool stop = true;
+    cout << "请选择加密(1)/解密(2)" << endl;
+    int start;
+    cin >> start;
+    switch (start) {
+    case 1: {
+        while (stop) {
+            string message;
+            getline(cin, message);
+            for (int i = 0; i < read.size(); i++) {
+                if (message == read[i]) {
+                    cout << (i ^ input1) % input2 << endl;
+                    i = read.size();
+                }
+                else if (message == "stop") {
+                    stop = false;
+                }
+            }
+        }
+        break;
+    }
+    case 2: {
+        while (stop) {
+            int message;
+            cin >> message;
+            if (message < 0) {
+                stop = false;
+            }
+            else {
+                cout << read[(message ^ input1) % input2] << endl;
+            }
+        }
+        break;
+    }
+    default:
+        stop = false;
+        break;
+    }
+    return 0;
+}
